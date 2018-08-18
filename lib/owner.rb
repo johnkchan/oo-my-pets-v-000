01d6321d2a6865b7117428d3d2571b1543395d7a
Jonathan Chan <johnkchan252@gmail.com>
2018-08-18 21:05:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,10 +46,14 @@ class Owner
   
   def play_with_cats()
     @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
   
   def feed_fish()
-    
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
   
   def sell_pets()
